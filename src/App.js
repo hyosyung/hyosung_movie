f77adad2599a7baa8b771cb5b4271e9b0ae1fc58
hyosyung <melon940925@naver.com>
2020-07-02 13:33:14 +0900

    map함수를 이용하여 여러가지 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-function Food({ fav }) {
-  return <h1> I love {fav} </h1>;
+function Food({ name,picture }) {
+  return <div>
+      <h2>I like {name}</h2>
+      <img src={picture} width="300px" height="300px"/>
+      </div>;
 }
 
 const foodLike=[
@@ -13,12 +16,13 @@ const foodLike=[
 ];
 //props=컴포넌트로 데이터를 보낼 때 사용할 수 있는 방법
 
+
 function App() {
   //App이라는 컴포넌트를 정의한다.
   // props에 있는 데이터는 문자열일 경우를 제외하면 모두 중괄호로 감싸야 함!
   return (
     <div>
-      <h1> Hello </h1> <Food fav="kimchi" />
+        {foodLike.map(dish=>(<Food name={dish.name} picture={dish.image}/>))}
     </div>
   );
 }

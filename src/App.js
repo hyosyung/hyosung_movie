37dc7f329fbbd362b6dc395c4054ea0f4fabb704
hyosyung <melon940925@naver.com>
2020-07-10 14:48:50 +0900

    줄정리 (alt + shift + f)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 class App extends React.Component{//state를 사용하기 위해서 클래스형 컴포넌트를 작성할 것임
-
+  
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 //ReactDOM.render 함수의 동작 원리 -> App.js의 컴포넌트를 아이디가 root인 엘리먼트에 그린다.
-ReactDOM.render( <
-    App / > ,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));

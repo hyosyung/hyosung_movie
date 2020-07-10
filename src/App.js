3ae4609fb30b03f09f05d196a34d3fc01e4b2eca
hyosyung <melon940925@naver.com>
2020-07-10 16:49:19 +0900

    생명주기 함수

--- src/App.js
+++ src/App.js
@@ -3,19 +3,41 @@ import React from "react";
 class App extends React.Component {
   //state를 사용하기 위해서 클래스형 컴포넌트를 작성할 것임
   //React.Component 클래스로부터 상속을 받음
+  constructor(props){
+    super(props);
+    console.log('Hello');
+  }
+
+  componentDidMount(){
+    console.log("component rendered");
+  }
+
+  componentDidUpdate(){
+    console.log('I just updated');
+  }
+
+  componentWillUnmount(){
+    console.log("Goodbye");
+  }
+
   state = {
     count: 0,
   };
 
-  add=()=>{
-    this.setState({count:1});
-  }
+  add = () => {
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
 
-  Minus=()=>{
-    this.setState({count:-1});
-  }
+  Minus = () => {
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
+  };
 
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>This number is : {this.state.count}</h1>

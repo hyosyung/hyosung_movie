e7637c96f7ecbb60d93539f2721bf0cd24378703
hyosyung <melon940925@naver.com>
2020-07-14 12:05:28 +0900

    Movie의 prop-types 저장!

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-function Movie(){
-    return <h1></h1>;
+function Movie() {
+  return <h1></h1>;
 }
 
-Movie.propTypes={};
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+}; //Movie의 prop-types 저장!
 
 export default Movie;

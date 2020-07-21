c04caff21bdfc19a9dcb6c91a6167df6acb83bb2
hyosyung <melon940925@naver.com>
2020-07-21 14:51:45 +0900

    상세정보 화면으로 이동하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
-
+//라우터의 사용법을 알아두기!
 function App() {
   return (
     <HashRouter>
       <Navigation/>
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail}/>
     </HashRouter>
   );
 }

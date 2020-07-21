23b8fdb877a40f2c8b1febb0d2c03b646bd51efa
hyosyung <melon940925@naver.com>
2020-07-21 13:51:52 +0900

    about.js 내용 추가

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <span>김효성이 리액트를 이용하여 만든 영화 페이지입니다.</span>;
+  return (
+    <div className="about__container">
+      <span>
+        이 웹사이트는 김효성이 처음으로 리액트를 이용하여 만들어 본 영화 앱
+        페이지입니다.
+      </span>
+      <span>문의 : 010-8695-3456</span>
+    </div>
+  );
 }
-
 export default About;

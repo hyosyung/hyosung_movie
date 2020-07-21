d89affbb907fd615e05e291bae3dee1af971e0bc
hyosyung <melon940925@naver.com>
2020-07-21 14:47:48 +0900

    네비게이션 완료

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,9 +6,7 @@ function Navigation() {
   return (
     <div className="nav">
       <Link to="/">Home</Link>
-      <Link to="./about">About
-        About
-      </Link>
+      <Link to="./about">About</Link>
     </div>
   );
 }

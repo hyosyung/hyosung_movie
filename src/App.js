3e74b09bc0415e65fb7457d186ffbd07ee2c71e5
hyosyung <melon940925@naver.com>
2020-07-21 13:16:39 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import './App.css';
+import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
-const List_movies = `https://yts-proxy.now.sh/list_movies.json?sort_by=rating`;
-
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      }, //받아온 객체 안에 접근하여 가져오기!! movies라는 이름을 그대로 쓸 수 있다
-    } = await axios.get(List_movies);
-    //await가 끝난 뒤에 setstate가 실행-> isLoading이 false로 바뀜
-    this.setState({ movies, isLoading: false }); //movies:movies를 movies로 축약할 수 있다!
-  };
-
-  componentDidMount() {
-    this.getMovies();
-    //개발자 도구 -> network탭 -> name의 json 확인!
-  }
-
-  render() {
-    const { isLoading, movies } = this.state; //구조분해 할당 문법!
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader text">"Loading..."</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    ); //중괄호 안에 삼항 연산자
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
+
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <span>김효성이 리액트를 이용하여 만든 영화 페이지입니다.</span>;
+}
+
+export default About;

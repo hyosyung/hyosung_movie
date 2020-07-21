32686228e308aef3d8328260fe897e7b5be16c39
hyosyung <melon940925@naver.com>
2020-07-21 15:01:29 +0900

    detail 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,13 +3,18 @@ import React from "react";
 class Detail extends React.Component {
   componentDidMount() {
     const { location, history } = this.props;
-    if(location.state===undefined){
-        history.push('/');
+    if (location.state === undefined) {
+      history.push("/");
     }
   }
 
   render() {
-    return <span>hello</span>;
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
   }
 }
 export default Detail;
